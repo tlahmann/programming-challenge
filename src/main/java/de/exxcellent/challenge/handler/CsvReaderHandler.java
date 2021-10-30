dad6409c9e29ccc72f64a6506a1078cdfea3441a
tlahmann <tobiaslahmann@gmail.com>
2021-10-30 13:20:26 +0200

    feat(csvreader): add csv reader method stub

---
+++ src/main/java/de/exxcellent/challenge/handler/CsvReaderHandler.java
@@ -0,0 +1,14 @@
+package de.exxcellent.challenge.handler;
+
+import java.util.List;
+
+public class CsvReaderHandler implements IHandler<String, List<String>> {
+    @Override
+    public void validate(String filePath) {
+    }
+
+    @Override
+    public List<String> process(String filePath) {
+        return null;
+    }
+}
