00235b5de02392064ebb341f27798ab702b8777d
tlahmann <tobiaslahmann@gmail.com>
2021-10-30 13:35:04 +0200

    feat: add csv file read

--- src/main/java/de/exxcellent/challenge/handler/CsvReaderHandler.java
+++ src/main/java/de/exxcellent/challenge/handler/CsvReaderHandler.java
@@ -2,7 +2,21 @@ package de.exxcellent.challenge.handler;
 
 import java.util.List;
 
+/**
+ * The csv reader implements a handler. It expects a string representing the
+ * file path to a csv file. This path is importet and returned as a list of
+ * strings.
+ * 
+ * @author <a href="https://github.com/tlahmann">Tobias Lahmann</a>
+ */
 public class CsvReaderHandler implements IHandler<String, List<String>> {
+    /**
+     * Validates the given input string. The csv reaer expects a non empty string
+     * with the '.csv' file extension
+     * 
+     * @throws IllegalArgumentException if the given input is null, empty or does
+     *                                  not end with '.csv'
+     */
     @Override
     public void validate(String filePath) throws IllegalArgumentException {
         if (filePath == null || filePath.isEmpty()) {
@@ -12,8 +26,15 @@ public class CsvReaderHandler implements IHandler<String, List<String>> {
         }
     }
 
+    /**
+     * Reads the given file path and returns a list of strings.
+     * @param filePath the file path to the csv file
+     * @return a list of strings representing the csv file
+     */
     @Override
     public List<String> process(String filePath) {
+        this.validate(filePath);
+        filePath = getClass().getClassLoader().getResource(filePath).getFile();
         return null;
     }
 }
