a25bf129f9a5a8a89bce5e388b8001e53ae84cc4
tlahmann <tobiaslahmann@gmail.com>
2021-10-30 13:32:16 +0200

    feat: add csv reader validation

--- src/main/java/de/exxcellent/challenge/handler/CsvReaderHandler.java
+++ src/main/java/de/exxcellent/challenge/handler/CsvReaderHandler.java
@@ -4,7 +4,12 @@ import java.util.List;
 
 public class CsvReaderHandler implements IHandler<String, List<String>> {
     @Override
-    public void validate(String filePath) {
+    public void validate(String filePath) throws IllegalArgumentException {
+        if (filePath == null || filePath.isEmpty()) {
+            throw new IllegalArgumentException("File path is empty");
+        } else if (!filePath.endsWith(".csv")) {
+            throw new IllegalArgumentException("File path is not a csv file");
+        }
     }
 
     @Override
